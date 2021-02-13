611ccf5c889009270adf3a05099f5e22a5b42d5a
TianjieJia <tjia@purdue.edu>
2021-02-13 17:31:20 -0500

    Jerry with setting menu

--- Assets/Scripts/GUIScript.cs
+++ Assets/Scripts/GUIScript.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class GUIScript : MonoBehaviour
 {
     public static bool gamePaused = false;
-    public GameObject pauseMenuUI;
+    public GameObject pauseMenuPanel;
+    public GameObject settingPanel;
 
     public GameObject timeDisplay;
     public int secondLeft = 30;
     public bool timeGone = false;
 
-    public Slider slider;
+    public Slider healthBar;
+
+    public AudioMixer audioMixer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -50,7 +55,7 @@ public class GUIScript : MonoBehaviour
     public void Resume()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        pauseMenuUI.SetActive(false);
+        pauseMenuPanel.SetActive(false);
         Time.timeScale = 1f;
         gamePaused = false;
     }
@@ -58,7 +63,7 @@ public class GUIScript : MonoBehaviour
     void Pause()
     {
         Cursor.lockState = CursorLockMode.None;
-        pauseMenuUI.SetActive(true);
+        pauseMenuPanel.SetActive(true);
         Time.timeScale = 0f;
         gamePaused = true;
     }
@@ -99,12 +104,30 @@ public class GUIScript : MonoBehaviour
 
     public void SetHealth(int health)
     {
-        slider.value = health;
+        healthBar.value = health;
     }
 
     public void SetMaxHealth(int health)
     {
-        slider.maxValue = health;
-        slider.value = health;
+        healthBar.maxValue = health;
+        healthBar.value = health;
+    }
+
+    public void GoSettingMenu()
+    {
+        pauseMenuPanel.SetActive(false);
+        settingPanel.SetActive(true);
+    }
+
+    public void ReturnFromSetting()
+    {
+        settingPanel.SetActive(false);
+        pauseMenuPanel.SetActive(true);
+    }
+
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+        audioMixer.SetFloat("Volume", volume);
     }
 }
