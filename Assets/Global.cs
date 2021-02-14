e2e16941b2bd8a9e448867f5c0be77b86a472cd5
ericeschnei <ericeschnei@gmail.com>
2021-02-14 03:19:05 -0500

    merge fixes

--- Assets/Global.cs
+++ Assets/Global.cs
@@ -22,6 +22,8 @@ public class Global : MonoBehaviour
 
     public GUIScript GUICanvas;
 
+    public List<PickupLocation> depots;
+    
     // Start is called before the first frame update
     void Awake()
     {
