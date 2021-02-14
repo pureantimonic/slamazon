49f0521a2bd7afd4df3479d3375e6c8d091f65ee
ericeschnei <ericeschnei@gmail.com>
2021-02-13 19:18:14 -0500

    Box Icons

--- Assets/Scripts/Package.cs
+++ Assets/Scripts/Package.cs
@@ -6,11 +6,11 @@ using UnityEngine.UI;
 
 public class Package : MonoBehaviour
 {
-    [SerializeField] private GameObject iconCanvas;
-    [SerializeField] private GameObject boxIcon;
+    private GameObject packageIcon;
+    private GameObject destinationIcon;
+
     [SerializeField] private float iconMargin;
     [SerializeField] private float minVelocity = 0.1f;
-    private GameObject m_BoxIcon;
     private Rigidbody rbody;
     private bool waitingForRest;
     
@@ -18,35 +18,46 @@ public class Package : MonoBehaviour
     {
         rbody = GetComponent<Rigidbody>();
         iconCanvas = Global.Instance.iconCanvas;
-        m_BoxIcon = Instantiate(boxIcon, iconCanvas.transform, false) as GameObject;
-        m_BoxIcon.GetComponent<Image>().color = UnityEngine.Random.ColorHSV(0f, 1f, 0.8f, 1.0f, 0.5f, 1.0f);
-    }
-    
-    public void Update()
-    {
-        var screenPoint = Camera.main.WorldToScreenPoint(this.transform.position);
-        var canvasTransform = iconCanvas.transform as RectTransform;
-        
 
-        var width = canvasTransform.rect.width;
+        packageIcon = gameObject.transform.Find("PackageIcon").gameObject;
+        destinationIcon = gameObject.transform.Find("DestinationIcon").gameObject;
+        
+        packageIcon.transform.SetParent(iconCanvas.transform, false);
+        destinationIcon.transform.SetParent(iconCanvas.transform, false);
+
+        Color color = UnityEngine.Random.ColorHSV(0f, 1f, 0.8f, 0.8f, 0.8f, 0.8f);
+        packageIcon.GetComponent<Image>().color = color;
+        destinationIcon.GetComponent<Image>().color = color;
+    }
+
+    public void UpdateIcon(Vector3 position, GameObject obj)
+    {
+        var screenPoint = Camera.main.WorldToScreenPoint(position);
+        var canvasTransform = iconCanvas.transform as RectTransform;
+
+        var w = canvasTransform.rect.width;
+        var h = canvasTransform.rect.height;
 
         if (screenPoint.z < 0)
         {
             screenPoint.x = Mathf.Sign(screenPoint.x) * Mathf.NegativeInfinity;
         }
 
-        m_BoxIcon.transform.position = new Vector3(
-            Mathf.Clamp(screenPoint.x, width * iconMargin, width * (1f - iconMargin)),
-            canvasTransform.rect.height * 0.9f
-        );
-        
-        
-        
+        obj.transform.position = new Vector3(
+            Mathf.Clamp(screenPoint.x, w * iconMargin, w * (1f - iconMargin)),
+            h * 0.9f);
+    }
+
+    public void Update()
+    {
+        UpdateIcon(this.transform.position, packageIcon);
+        UpdateIcon(this.destination, destinationIcon);
     }
 
     public void OnDestroy()
     {
-      //  Destroy(m_BoxIcon);
+        Destroy(packageIcon);
+        Destroy(destinationIcon);
     }
 
     [SerializeField] public Vector3 anchorPoint;
@@ -54,7 +65,6 @@ public class Package : MonoBehaviour
 
     public void OnPickedUp()
     {
-        m_BoxIcon.SetActive(false);
         waitingForRest = false;
         if (pl)
         {
@@ -65,13 +75,11 @@ public class Package : MonoBehaviour
 
     public void OnRelease()
     {
-        m_BoxIcon.SetActive(true);
         waitingForRest = true;
     }
 
     public void OnRest()
     {
-        Destroy(m_BoxIcon);
         Global.Instance.AddScore(10 - Vector3.Distance(transform.position, destination));
         Global.Instance.AddPackage();
         Destroy(gameObject);
@@ -112,5 +120,6 @@ public class Package : MonoBehaviour
     public PickupLocation pl;
     public Vector3 destination;
     public MeshRenderer mainMesh;
+    public GameObject iconCanvas;
 
 }
